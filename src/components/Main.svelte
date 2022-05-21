6e66fbaca0bb157d9b7eaaab6598005183bff566
Ryan M <46380964+anarchylimes@users.noreply.github.com>
2022-05-21 19:37:17 -0400

    Test 4
    Test 4

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,6 +6,18 @@
     width: 100%;
     padding: 1rem;
     margin: 2rem auto;
+
+  }
+
+  .noise {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url(/images/noise.gif);
+    opacity: .05;
+    pointer-events: none;
   }
 </style>
 <main>
