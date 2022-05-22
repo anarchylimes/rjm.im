ad4fb52331f196da38ebe91b199690840168a6f3
Ryan M <46380964+anarchylimes@users.noreply.github.com>
2022-05-22 04:36:39 -0400

    js
    js

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,43 @@
 <script>
+  import { onMount } from 'svelte'
+  import Letterize from 'letterizejs'
+
+  onMount(() => {
+    const letters = new Letterize({
+      targets: 'h1'
+    })
+    const setRandomSpan = () => {
+      let random
+      const spans = document.querySelectorAll('h1 > span')
+      spans.forEach(span => {
+        random = Math.random()
+        if (random > .6) {
+          span.classList.toggle('flipped')
+        }
+      })
+    }
+    setRandomSpan()
+    return () => {
+      clearInterval(interval)
+    }
+  })
 </script>
 
-<h1>Hi! I'm Ryan</h1>
+<h1 class="intro">Hi! I'm Ryan</h1>
 
 <style>
   h1 {
     letter-spacing: -0.025rem;
     font-weight: normal;
+    font-size: 20px;
   }
+  :global(span) {
+    line-height: 25px;
+  }
+  :global(.flipped) {
+    transform: rotate(180deg);
+    display: inline-block;
+
+  }
+
 </style>
